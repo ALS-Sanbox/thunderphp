565728833c3db9797d8dde0d9d89215091272e76
ALS-Sanbox <ilovetylila@gmail.com>
2025-04-15 15:10:27 -0500

    Update view-sample.php

--- app/thunder/samples/view-sample.php
+++ app/thunder/samples/view-sample.php
@@ -8,3 +8,25 @@
         <p><?= htmlspecialchars($message) ?></p>
     </center>
 </div>
+
+<div>
+    <h1>{{ title }}</h1>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            {% for record in records %}
+                <tr>
+                    <td>{{ record.id }}</td>
+                    <td>{{ record.name }}</td>
+                    <td>{{ record.email }}</td>
+                </tr>
+            {% endfor %}
+        </tbody>
+    </table>
+</div>
