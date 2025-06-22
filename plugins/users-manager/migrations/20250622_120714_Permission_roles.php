7fae6845db0846c88c4cbc29e19536ae6d544bd1
AfroLion <ilovetylila@gmail.com>
2025-06-22 16:14:06 -0500

    Create 20250622_120714_Permission_roles.php

---
+++ plugins/users-manager/migrations/20250622_120714_Permission_roles.php
@@ -0,0 +1,27 @@
+<?php
+
+use Migration\Migration;
+
+defined('FCPATH') or die("Direct script access denied");
+
+/**
+ * Permission_roles class
+ */
+class Permission_roles extends Migration {
+    public function up() {
+        $this->addColumn('id int unsigned auto_increment');
+        $this->addColumn('role_id tinyint(1) unsigned default 0');
+        $this->addColumn('permission varchar(255) not null unique');
+        $this->addColumn('disabled tinyint(1) unsigned default 0');
+
+        $this->addPrimaryKey('id');
+        $this->addKey('role_id');
+        $this->addKey('permission');
+
+        $this->createTable('permission_roles');
+    }
+
+    public function down() {
+        $this->dropTable('permission_roles');
+    }
+}
