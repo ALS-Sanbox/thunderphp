4b58085a783ae9148831f428515943f12fb194c3
ALS-Sanbox <ilovetylila@gmail.com>
2026-08-10 12:39:37 -0500

    Stop tracking config.php (contained live DB credentials); add config-sample.php template

--- config-sample.php
+++ config-sample.php
@@ -6,38 +6,38 @@ define('APP_DESCRIPTION', 'An Plugin Based PHP Framework, Everything here is an 
 if((empty($_SERVER['SERVER_NAME']) && strpos(PHP_SAPI, 'cgi') !== 0) || (!empty($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'localhost'))
 {
 	/** The name of your database */
-    define( 'DB_NAME', 'thunder_thunderphp_db');
+    define( 'DB_NAME', 'your_db_name');
 
     /** Database username */
-    define( 'DB_USER', 'thunder_tp');
+    define( 'DB_USER', 'your_db_user');
 
     /** Database password */
-    define( 'DB_PASSWORD', 'R@inbow2025');
+    define( 'DB_PASSWORD', 'your_db_password');
 
     /** Database hostname */
     define( 'DB_HOST', 'localhost');
 
     /** Database driver */
     define( 'DB_DRIVER', 'mysql');
-	
-	define('ROOT', 'http://45-79-8-243.ip.linodeusercontent.com');
+
+	define('ROOT', 'http://localhost');
 
 }else
 {
 	/** The name of your database */
-    define( 'DB_NAME', 'thunder_thunderphp_db');
+    define( 'DB_NAME', 'your_db_name');
 
     /** Database username */
-    define( 'DB_USER', 'thunder_tp');
+    define( 'DB_USER', 'your_db_user');
 
     /** Database password */
-    define( 'DB_PASSWORD', 'R@inbow2025');
+    define( 'DB_PASSWORD', 'your_db_password');
 
     /** Database hostname */
     define( 'DB_HOST', 'localhost');
 
     /** Database driver */
     define( 'DB_DRIVER', 'mysql');
-	
-	define('ROOT', 'http://45-79-8-243.ip.linodeusercontent.com');
+
+	define('ROOT', 'https://your-domain.example.com');
 }
